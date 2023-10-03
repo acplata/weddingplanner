7e614a1423efdba4833ba6609d0eab27233bef63
Ana Plata <acplata@Anas-MacBook-Air.local>
2023-10-02 20:22:33 -0400

    final details

---
+++ src/front/js/component/ProfilePregFrecuentes.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const ProfilePregFrecuentes = () => {
+    return (
+        <div className=" profile-row container overflow-y-scroll tab-pane fade" id="qa-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabIndex="0">
+
+            <div className="profile-guide-layout mt-2">
+                <div className="card-profile-guia">
+                    <div className="card-body">
+                        <h5 className="card-title"><strong>¿Con qué antelación debería comenzar la planificación?</strong></h5>
+                        <p className="card-text mt-3">Al menos 6 meses. La mayoría de los lugares se reservan con un año de anticipación en Dubai, al igual que los fotógrafos, artistas e incluso floristas. Por lo tanto, darse suficiente tiempo por adelantado le brindará muchas más opciones a la hora de elegir proveedores y lugar.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="profile-guide-layout mt-2">
+                <div className="card-profile-guia">
+                    <div className="card-body">
+                        <h5 className="card-title"><strong>¿Son necesarios los temas de boda? ¿Cómo te decides por uno?</strong></h5>
+                        <p className="card-text mt-3">Los temas de boda pueden ser muy complicados. Un tema debe planificarse y ejecutarse a la perfección; de lo contrario, puede resultar contraproducente. Para desarrollar el tema, debes ejecutarlo con la paleta de colores adecuada y elementos individuales para crear el ambiente y la sensación adecuados. Un tema ayuda a unir todo.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="profile-guide-layout mt-2">
+                <div className="card-profile-guia">
+                    <div className="card-body mb-2">
+                        <h5 className="card-title"><strong>¿Son necesarios los obsequios de boda?</strong></h5>
+                        <p className="card-text mt-3">Si bien los obsequios de boda no son imprescindibles, es un buen gesto enviar a sus invitados a casa con algo que les recuerde su boda. Trabaje dentro de su presupuesto y habrá muchas ideas que son asequibles pero que dejan una impresión duradera.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/front/js/pages/Proyecto.jsx
+++ src/front/js/pages/Proyecto.jsx
@@ -4,7 +4,23 @@ export const Proyecto = () => {
 
     return (
         <div>
-            <h3 className="m-5">Wedding Planner Interactive Website</h3>
+            <div className="container">
+                <div className="container d-flex justify-content-center align-items-center mb-3 mt-5 text-center">
+                    <div className="gap-3">
+                        <h1><strong>A+M Wedding Planner</strong></h1>
+                        <h3>10 AÑOS DE EXPERIENCIA Y CONTANDO</h3>
+                    </div>
+                </div>
+                <div className="mb-5 mt-5 m-5 text-center">
+                    <p>Somos una página web interactiva multifacética que le brinda acceso privilegiado a las personas que quieren planificar su matrimonio desde cero sin la necesidad de un wedding planner real. Adicionalmente, le ofrecemos a los proveedores un espacio para dar a conocer sus servicios.</p>
+                </div>
+                <div className="mb-5 mt-5 m-5 text-center">
+                    <p>Nuestro objetivo es reimaginar la planificacion de matrimonios y llevarlo detrás de escena con quienes lo saben. Celebramos la expresión independiente, en todas sus variadas formas, y ayudamos a allanar el camino para lo nuevo y lo próximo.</p>
+                </div>
+                <div className="mb-5 mt-5 m-5 text-center">
+                    <p>¿Quieres trabajar con nosotros? Explora nuestras oportunidades profesionales actuales.</p>
+                </div>
+            </div>
         </div>
     )
 }
